07fd9b6d7595735830136efb0377da652799edc2
dossier123 <160337571+dossier123@users.noreply.github.com>
2024-08-27 03:21:02 +0530

    Update execution_client.py

--- execution_client.py
+++ execution_client.py
@@ -7,26 +7,15 @@ class ExecutionException(Exception):
 
 class ExecutionClient(Protocol):
 
-
-    def __init__(self):
-        self.orders = {}
-
-    def buy(self, product_id: str, amount: int, order_type: str, limit_price: int):
+    def buy(self, product_id: str, amount: int):
         """
         Execute a buy order, throws ExecutionException on failure
         :param product_id: the product to buy
         :param amount: the amount to buy
         :return: None
         """
-        if self.should_execute(order_type, limit_price, amount):
-            self.orders[product_id] = {
-                'type': order_type,
-                'amount': amount,
-                'limit_price': limit_price,
-                'executed': True
-            }
-        else:
-            raise ExecutionException
+        print(f"Order: {product_id} is bought for amount: {amount}.")
+        ...
 
     def sell(self, product_id: str, amount: int):
         """
@@ -35,17 +24,5 @@ class ExecutionClient(Protocol):
         :param amount: the amount to sell
         :return: None
         """
-        order = self.orders.get(product_id)
-        if self.should_execute(order['type'], order['limit_price'], amount):
-            del self.orders[product_id]
-        else:
-            raise ExecutionException
-
-
-    def should_execute(self, order_type, limit_price, price):
-        """Check if the order should be executed based on the current price."""
-        if order_type == 'buy' and price <= limit_price:
-            return True
-        if order_type == 'sell' and price >= limit_price:
-            return True
-        return False
+        print(f"Order: {product_id} is sold for amount: {amount}.")
+        ...
