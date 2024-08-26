6e3cf6b5aa720ec29035cf7cea37afd468a54c48
dossier123 <160337571+dossier123@users.noreply.github.com>
2024-08-27 03:20:03 +0530

    Update limit_order_agent.py

--- limit_order_agent.py
+++ limit_order_agent.py
@@ -1,37 +1,62 @@
-from trading_framework.execution_client import ExecutionClient, ExecutionException
+import sys, os
+sys.path.append("..")
+from trading_framework.execution_client import ExecutionClient
 from trading_framework.price_listener import PriceListener
 
 
 class LimitOrderAgent(PriceListener):
-
     def __init__(self, execution_client: ExecutionClient) -> None:
-        """
-
-        :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
-        """
+        # self.ibm_limit_price = 100
+        # self.ibm_order_placed = False
         self.execution_client = execution_client
-        self.ibm_limit_price = 100
-        self.ibm_order_placed = False
+        self.orders = []
         super().__init__()
 
-    def on_price_tick(self, product_id: str, price: float):
+    def add_order(self, product_id, order_operation, amount, price_limit):
+        if order_operation not in {"buy","sell"}:
+            raise Exception("Wrong Order Added, Please Check!")
+
+        if amount < 0 or price_limit < 0:
+            raise ValueError("Amount/Limit should be greater than zero.")
+
+        if not isinstance(amount, (int)) and not isinstance(price_limit, (int)):
+            raise ValueError("Amount/Limit should be integer.")
+
+        self.orders.append({
+            'order_operation': order_operation,
+            'product_id': product_id,
+            'amount': amount,
+            'price_limit': price_limit
+        })
+
+    def on_price_tick(self, product_id, price):
         # see PriceListener protocol and readme file
-        if product_id == "IBM" and price < self.ibm_limit_price and not self.ibm_order_placed:
-            self.execution_client.execute_order("BUY", "IBM", 1000)
-            self.ibm_order_placed = True
+        for order in self.orders[:]:
 
-    def add_order(self, order_type: str, product_id: str, amount: int, limit_price: float):
-        """Add an order to the list of orders."""
-        if order_type not in {'buy', 'sell'}:
-            raise ExecutionException("Order side must be 'buy' or 'sell'.")
-        if amount <= 0:
-            raise ExecutionException("Amount must be greater than 0.")
-        if limit_price <= 0:
-            raise ExecutionException("Limit price must be greater than 0.")
+            if order['product_id'] == product_id:
+                if order['order_operation'] == 'buy' and price <= order['price_limit']:
+                    self.execution_client.buy(product_id, order['amount'])
 
-        ExecutionClient.orders[product_id] = {
-                'type': order_type,
-                'amount': amount,
-                'limit_price': limit_price,
-                'executed': True
-        }
+                    self.orders.remove(order)
+
+                if order['order_operation'] == 'sell' and price >= order['price_limit']:
+                    self.execution_client.sell(product_id, order['amount'])
+
+                    self.orders.remove(order)
+
+    # def price_tick(self, product_id, price):
+    #     if product_id == 'IBM' and price < self.ibm_limit_price and not self.ibm_order_placed:
+    #         self.execution_client.buy('IBM', 1000)
+    #         self.ibm_order_placed = True
+
+class MockingExecutionClient(ExecutionClient):
+    pass
+
+
+order_obj = MockingExecutionClient(_exec)
+order_obj.add_order("product_x","buy", 500,100)
+order_obj.add_order("product_y","buy", 1000,10)
+order_obj.add_order("product_z","sell", 950,150)
+order_obj.on_price_tick("product_x",90)
+order_obj.on_price_tick("product_y",90)
+order_obj.on_price_tick("product_z",200)
