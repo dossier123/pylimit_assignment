6520ab9a558468e52175a61be11ddd43d457d5f5
dossier123 <160337571+dossier123@users.noreply.github.com>
2024-08-27 03:18:57 +0530

    Update price_listener.py

--- price_listener.py
+++ price_listener.py
@@ -1,5 +1,4 @@
 from typing import Protocol
-from execution_client import ExecutionClient as execution_client 
 
 
 class PriceListener(Protocol):
@@ -11,9 +10,4 @@ class PriceListener(Protocol):
         :param price: the current market price of hte product
         :return: None
         """
-        order = execution_client.orders.get(product_id)
-        if order['product_id'] == product_id and not order['executed']:
-                if (order['type'] == "BUY" and price <= order['limit']) or \
-                   (order['type'] == "SELL" and price >= order['limit']):
-                    execution_client.execute_order(order['type'], product_id, order['amount'])
-                    order['executed'] = True
+        ...
